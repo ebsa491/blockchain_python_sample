15fb3b29f161d50c3af0313d53938b35d231454c
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-11-14 10:13:30 +0330

    Blockchain class structure

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.current_trx = []
+
+    def new_block(self):
+        pass
+
+    def new_trx(self):
+        pass
+
+    @property
+    def last_block(self):
+        pass
+
+def hash_block(block):
+    pass
+
