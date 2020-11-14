21f0c2adfba0ec799aec2a316f0514fdfac02e29
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-11-14 10:25:03 +0330

    new_block method

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
 
+from time import time
+
+
 class Blockchain:
     def __init__(self):
         self.chain = []
-        self.current_trx = []
+        self.current_trxs = []
+        self.new_block(previos_hash=1, proof=100)
 
-    def new_block(self):
-        pass
+    def new_block(self, proof, previos_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'trxs': self.current_trxs,
+            'proof': proof,
+            'previos_hash': previos_hash or hash(self.chain[-1])
+        }
+        self.current_trxs = []
+        self.chain.append(block)
+        return block
 
     def new_trx(self):
         pass
@@ -15,6 +28,6 @@ class Blockchain:
     def last_block(self):
         pass
 
+
 def hash_block(block):
     pass
-
